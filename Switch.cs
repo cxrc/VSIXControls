9ad9b0bdab746816c9a56ca7e99a97328dec9c17
cxrc <63002560+cxrc@users.noreply.github.com>
2021-02-25 21:16:48 +0100

    Actualización SWITCH operativo

--- Switch.cs
+++ Switch.cs
@@ -10,12 +10,16 @@ namespace VSIXControls
     [ProvideToolboxControl("VSIXControls", false)]
     public partial class Switch : UserControl
     {
-        bool state = false;
-        bool colored;
-        Color fondoCheked = Color.Lime;
-        Color fondoUnchecked = Color.Red;
-        Color fondoChekedActual;
-        Color fondoUncheckedActual;
+        private bool state = false;
+        private bool colored;
+        private bool show10Characters = false;
+        private Color fondoCheked = Color.Lime;
+        private Color fondoUnchecked = Color.Red;
+        private Color show10Color = Color.Gray;
+        private Color fondoChekedActual;
+        private Color fondoUncheckedActual;
+        private int Angle = 0;
+
         public Switch()
         {
             InitializeComponent();
@@ -27,6 +31,10 @@ namespace VSIXControls
         public bool State { get => state; set { state = value; Invalidate(); } }
 
         public bool Colored { get => colored; set { colored = value; OnChangeColoredProperty(); } }
+
+        public bool Show10Characters { get => show10Characters; set { show10Characters = value; Invalidate(); } }
+
+        public Color Show10Color { get => show10Color; set { show10Color = value; Invalidate(); } }
 
         private void OnChangeColoredProperty()
         {
@@ -52,6 +60,7 @@ namespace VSIXControls
             int mandoX;
 
             // Borde
+
             Pen lapizNegro = new Pen(Color.Black);
             Pen lapizBlanco = new Pen(Color.White);
             Rectangle rectanguloBorde = new Rectangle(0, 0, Width - 1, Height - 1);
@@ -62,6 +71,7 @@ namespace VSIXControls
             gr.DrawLine(lapizNegro, 1, 1, Width - 2, 1);
 
             // Dibuja el fondo
+
             if (this.state)
             {
                 fondo = new SolidBrush(fondoChekedActual);
@@ -75,9 +85,8 @@ namespace VSIXControls
             Rectangle rectanguloInterior = new Rectangle(2, 2, Width - 4, Height - 4);
             gr.FillRectangle(fondo, rectanguloInterior);
 
-
             // Dibuja el Mando
-            //gr.DrawEllipse(lapizNegro, mandoX, 0, Height - 1, Height - 1);
+            
             int x = mandoX;
             int y0 = 2;
             int y1 = Height - 3;
@@ -88,6 +97,21 @@ namespace VSIXControls
                 if (x <= mandoX + Height - 3) gr.DrawLine(lapizGris, x, y0, x++, y1);
                 if (x <= mandoX + Height - 3) gr.DrawLine(lapizNegro, x, y0, x++, y1);
                 if (x <= mandoX + Height - 3) gr.DrawLine(lapizNegro, x, y0, x++, y1);
+            }
+
+            // 10
+            if (show10Characters)
+            {
+                Pen lapiz10 = new Pen(show10Color, 2F);
+                const int margen = 15;
+                if (state)
+                {
+                    gr.DrawLine(lapiz10, Height / 2, margen, Height / 2, Height - margen);
+                }
+                else
+                {
+                    gr.DrawEllipse(lapiz10, Width - Height + margen, margen, Height - margen*2, Height - margen*2);
+                }
             }
         }
 
